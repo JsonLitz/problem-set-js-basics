664deec43b1e0c4fbbed81b2e7d07178cd4910fd
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-06-29 17:51:37 -0700

    bottles and primes

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,14 @@
 */
 
 // YOUR CODE HERE
+
+
+function countDown (bottles) {
+  for (var i = bottles; i >= 0; i-- ) {
+    console.log(i + " bottles of beer on the wall,");
+    console.log(i + " bottles of beer!");
+    console.log(" Take one down and pass it around,");
+    console.log(i-1 + " bottles of beer on the wall...");
+  }
+}
+countDown()
